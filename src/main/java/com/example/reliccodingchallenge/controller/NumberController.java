78b896fa79d4a6002015b57ea05788928280ea6c
Christopher Burnette <christopher.burnette024@gmail.com>
2024-07-19 12:08:42 -0600

    Uploaded RestController Setup

--- src/main/java/com/example/reliccodingchallenge/controller/NumberController.java
+++ src/main/java/com/example/reliccodingchallenge/controller/NumberController.java
@@ -4,6 +4,7 @@ import com.example.reliccodingchallenge.dto.ConfirmationResponse;
 import com.example.reliccodingchallenge.dto.NumberRequest;
 import com.example.reliccodingchallenge.service.NumberService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class NumberController {
 
     private NumberService numberService;
+
+    @Autowired
     public NumberController(NumberService numberService) {
         this.numberService = numberService;
     }
@@ -22,9 +25,9 @@ public class NumberController {
     @MessageMapping("/submitNumber")
     @SendTo("/topic/confirmation")
     public ConfirmationResponse copyToList(@Valid NumberRequest numberInput, SimpMessageHeaderAccessor headerAccessor) throws InterruptedException {
-        Thread.sleep(1000);
+        Thread.sleep(1000); 
         numberService.processNumber(numberInput.number());
-        return new ConfirmationResponse("");
+        return new ConfirmationResponse("Number Accepted.");
     }
 
 }
